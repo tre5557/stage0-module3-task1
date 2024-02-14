c27ce732d7f8be2a97c2788b22fba815235a50e1
Timur Bashirov <bashirov188@gmail.com>
2024-02-14 20:01:24 +0300

    done

--- src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/FillGapsApp.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.module3.task1;
 
 public class FillGapsApp {
     public static void main(String[] args) {
-              .        ("Fixed");
+              System.out.println("Fixed");
+
     }
 }
